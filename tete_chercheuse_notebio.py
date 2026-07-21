54b17a6acfe12dae69cef5a36781fdf1920bb95b
adrien <savarseno@gmail.com>
2026-07-21 14:27:23 -0400

    màj gitignore, tentative ajout et suppression header dans sample. filtre des xml à considérer dans script

--- tete_chercheuse_notebio.py
+++ tete_chercheuse_notebio.py
@@ -4,4 +4,18 @@ import pandas as pd
 import xml.etree.ElementTree as ET
 import csv
 
-# 
+# stocker les id des articles dans une liste
+# /!\ remplacer 'sample' par la liste complète au moment d'exécuter le vrai script
+df = pd.read_csv('sample.csv', names=['idar'])
+sample = df['idar'].tolist()
+
+# créer le jeu de données pour stocker les informations
+affiliations_notesbio = pd.DataFrame(columns=["IDU de l'article", "IDau notebio", "Contenu notebio", "IDau auteur", "Prénom", "Nom de famille", "Nom complet", "IDU reconstitué"])
+
+# ne considérer que les dossiers dont le nom se trouve dans la liste
+racine = Path("/home/adrien/Documents/erudit-affiliation-2025/xml_pour_notebio")
+chemins_xml_avec_notebio = [
+    fichier for fichier in racine.rglob('*')
+    if fichier.is_file() and fichier.parent.name in sample
+]
+
