4678f6ae84c31d19561270385f1e1d6c7ceea5e2
adrien <savarseno@gmail.com>
2026-07-28 14:44:21 -0400

    ajout fonction pour métadonnées auteur·ices

--- tete_chercheuse_notebio.py
+++ tete_chercheuse_notebio.py
@@ -5,7 +5,7 @@ import xml.etree.ElementTree as ET
 import csv
 
 # stocker les id des articles dans une liste
-# /!\ remplacer 'sample' par la liste complète au moment d'exécuter le vrai script
+# /!\ remplacer 'sample.csv' par la liste complète
 df = pd.read_csv('sample.csv', names=['idar'])
 articles = df['idar'].tolist()
 
@@ -17,13 +17,21 @@ ns = {'erudit': 'http://www.erudit.org/xsd/article'}
 
 def recuperer_xml_notebio(racine, articles):
     """Retourne une liste comprenant les chemins vers les documents XML
-    qui contienent au moins une balise <notebio>."""
+    qui contienent au moins une balise <notebio>.
+    
+    La variable "racine" passée à la fonction correspond au chemin de
+    l'emplacement des dossiers à trier.
+    La variable "articles" correspond à la liste d'ID uniques des articles
+    sans affiliation candidats à l'extraction du contenu des balises notebio."""
+
     chemins_xml_sans_affiliation = [
         fichier for fichier in racine.rglob('*')
         if fichier.is_file() and fichier.parent.name in articles
     ]
+
     # parser les documents XML pour récupérer ceux qui ont une balise notebio
     ar_nb = []
+
     for chemin in chemins_xml_sans_affiliation:
         article = ET.parse(chemin).getroot()
         if article.findall(".//erudit:notebio", ns) and chemin not in ar_nb:
@@ -35,18 +43,62 @@ print(f"{len(xml_avec_notebio)} articles avec notices récupérés.")
 
 def texte_notebio(notice):
     """Concatène tous les paragraphes de la notice biobibliographique
-    pour les regrouper dans un seul paragraphe suivi."""
+    pour les regrouper dans un seul paragraphe suivi.
+    
+    La variable "notice" passée à la fonction correspond à l'élément XML
+    contenant potentiellement plusieurs balises "alinea"."""
+
     alinea = notice.findall(".//erudit:alinea", ns)
+
     texte = []
+
     for a in alinea:
         texte.append(a.text)
     return ' '.join(texte)
 
+def metadonnees_au(xml, idau):
+    """Retourne les métadonnées des auteur·ices d'un article
+    en fonction de leur index dans l'article (e.g. au1, au2, etc.).
+    
+    La variable "xml" passée à la fonction correspond à l'arborescence
+    d'un document XML à examiner.
+    La variable "idau" correspond à l'identifiant unique, extrait de la
+    balise "idrefs" ou du contenu de l'attribut "id" de la balise "auteur"."""
+
+    prenom = xml.find(".//*[@id='%s']//erudit:prenom" % idau, ns).text
+
+    aut_nom = xml.find('.//erudit:autreprenom', ns)
+    if aut_nom:
+        aut_nom = aut_nom.text
+
+    nomfam = xml.find(".//*[@id='%s']//erudit:nomfamille" % idau, ns).text
+
+    return {
+        "idau": idau,
+        "prenom": prenom,
+        "autreprenom": aut_nom if aut_nom is not None else None,
+        "nomfamille": nomfam,
+        "nomcomplet": f"{prenom} {nomfam}".strip(),
+    }
+
+
+
+# trouver textes notebio et metadonnées auteur
 for chemin in xml_avec_notebio:
     xml = ET.parse(chemin).getroot()
-    for notebio in xml.findall(".//erudit:notebio", ns):
-        hey = texte_notebio(notebio)
-        print(hey)
+
+    for nb in xml.findall(".//erudit:notebio", ns):
+        nb_id = nb.get('idrefs')
+        notebio = texte_notebio(nb)
+        print(notebio)
+
+        autaires = xml.findall('.//erudit:auteur', ns)
+
+        for autaire in autaires:
+            au_id = autaire.get('id')
+            if nb_id == au_id:
+                metadonnees = metadonnees_au(xml, au_id)
+                print(metadonnees)
 
 # # récupérer les informations depuis le document XML
 # metadonnees_nb = {}
